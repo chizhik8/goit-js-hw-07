ec892fd8f8efa04415503bca6bdc5dfd788e88fa
chizhik8 <64300649+chizhik8@users.noreply.github.com>
2020-12-01 16:44:58 +0200

    JS hw#7 - 3

--- JS/task-7.js
+++ JS/task-7.js
@@ -4,7 +4,5 @@ const sizeControlRef = document.querySelector('#font-size-control');
 const textRef = document.querySelector('#text');
 
 sizeControlRef.addEventListener('input', event => { 
-    console.log(event.target.value);
     textRef.style.fontSize = `${event.target.value}px`;
-
 });
